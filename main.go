9b64bbfac96625970db1e9e13453d6ff364a5d98
Mateus Werneck <werneck.mateus@gmail.com>
2024-09-28 10:01:06 -0300

    Melhorando logo da Symplicity

--- main.go
+++ main.go
@@ -15,7 +15,11 @@ func main() {
 	server.Static("/static", "./static")
 
 	server.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{"Title": "Mateus Werneck", "CelcoinOpacity": "opacity-100"})
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Title":             "Mateus Werneck",
+			"CelcoinOpacity":    "opacity-100",
+			"SymplicityOpacity": "opacity-100",
+		})
 	})
 
 	server.GET("/recent-work/celcoin/logo", func(c *gin.Context) {
@@ -24,6 +28,14 @@ func main() {
 
 	server.GET("/recent-work/celcoin/animation", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "celcoin-animation.html", nil)
+	})
+
+	server.GET("/recent-work/symplicity/logo", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "symplicity-logo.html", gin.H{"SymplicityOpacity": "opacity-0"})
+	})
+
+	server.GET("/recent-work/symplicity/animation", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "symplicity-animation.html", nil)
 	})
 
 	if err := server.Run(":9010"); err != nil {
