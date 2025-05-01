3354df56bd61a1d53fbc1d9aa4cce301a595a88d
Mateus Werneck <werneck.mateus@gmail.com>
2025-05-01 10:14:22 -0300

    Corrigindo conexão com Redis após atualizar pacote

--- app/storage/session.go
+++ app/storage/session.go
@@ -10,13 +10,13 @@ import (
 
 func NewSessionStore() redis.Store {
 	store, err := redis.NewStore(
-        10,
-        "tcp",
-        os.Getenv("REDIS_HOST"),
-        os.Getenv("REDIS_PASSWORD"),
-        os.Getenv("REDIS_AUTH"),
-    )
-
+		10,
+		"tcp",
+		os.Getenv("REDIS_HOST"),
+		"",
+		os.Getenv("REDIS_PASSWORD"),
+		[]byte(os.Getenv("REDIS_AUTH")),
+	)
 	if err != nil {
 		tools.GlobalLogger.Error("Failed to create session store", "Error", err)
 		log.Fatal(err)
